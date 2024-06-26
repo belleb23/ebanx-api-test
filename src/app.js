75d0463d1c0655d00c7804a70ea3e53d6ebf4cc1
belleb23 <bellebernieri@gmail.com>
2024-06-26 20:32:16 -0300

    Implement POST /event endpoint to handle deposit event

--- src/app.js
+++ src/app.js
@@ -12,4 +12,49 @@ app.post("/reset", (req, res) => {
   res.status(200).send("OK");
 });
 
+app.post("/event", (req, res) => {
+    const { type, amount, origin, destination } = req.body;
+
+    if (type && amount !== undefined) {
+        if (type === "deposit") {
+            
+            if (!destination) {
+                return res.status(400).send("Destination account is required for deposit");
+            }
+
+            if (!accounts[destination]) {
+                accounts[destination] = 0;
+            }
+
+            accounts[destination] += amount;
+            events.push({ type, amount, destination });
+
+            return res.status(201).json({
+                destination: {
+                    id: destination,
+                    balance: accounts[destination]
+                }
+            });
+        } else {
+            return res.status(400).send("Invalid event type");
+        }
+    } else {
+        return res.status(400).send("Invalid request body");
+    }
+});
+
+app.get("/balance", (req, res) => {
+    const { account_id } = req.query;
+
+    if (!account_id) {
+        return res.status(400).send("Account ID is required");
+    }
+
+    if (accounts[account_id] !== undefined) {
+        res.status(200).send(accounts[account_id].toString());
+    } else {
+        res.status(404).send("0");
+    }
+});
+
 module.exports = { app };
